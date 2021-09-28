1b5aff89daba4e16ab2a411fe02a14eeb3344c01
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-09-28 12:39:40 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -78,3 +78,13 @@ async def index_files(bot, message):
                 await msg.edit(f'Error: {e}')
             else:
                 await msg.edit(f'Total {total_files} Saved To DataBase!')
+
+RATING = ["5.1 | IMDB", "6.2 | IMDB", "7.3 | IMDB", "8.4 | IMDB", "9.5 | IMDB", ]
+GENRES = ["fun, fact",
+         "Thriller, Comedy",
+         "Drama, Comedy",
+         "Family, Drama",
+         "Action, Adventure",
+         "Film Noir",
+         "Documentary"]
+
