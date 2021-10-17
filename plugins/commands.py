85f648b1b57ab805a2a26f2abd5c8d8601f10d3b
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-10-17 14:38:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -93,7 +93,9 @@ async def start(bot, cmd):
             )
         )
     else:
-        await cmd.reply_text(START_MSG,
+       await cmd.reply_photo(
+            photo="https://telegra.ph/file/7cfb09d344bdbe70a6367.jpg"
+            caption=START_MSG,
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
